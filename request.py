8e8dacd0ec2c0a9de161e63d241ee49175f7f20b
EC2 Default User <ec2-user@ip-172-31-21-55.ap-northeast-1.compute.internal>
2018-02-03 12:29:37 +0000

    modify

--- request.py
+++ request.py
@@ -1,5 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 url = "https://news.yahoo.co.jp/topics"
 res = requests.get(url)
 with open('html.text', 'w') as file:
     file.write(res.text)
+
+bs = BeautifulSoup(res.text, "lxml")
+topics = bs.select('.fl, .fr')
+news_topics = {}
+for news in topics:
+    topic = news.select('li')[0].text
+    news_topics[topic] = [news_topic.text for news_topic in news.select('li')[1:-2]]
+
+with open('fin.text', 'w') as fin:
+    fin.write(str(news_topics))
